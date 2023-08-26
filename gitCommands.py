a77a12cf6693f8facd1eb65d87122852fc1ef48a
unknown <ciracicemberk0@gmail.com>
2023-08-26 23:28:41 +0300

    i added again

---
+++ gitCommands.py
@@ -0,0 +1,27 @@
+#                                                   git clone
+#command: git clone {https of the repository}. with this command you will download the git repository to your local environment
+#
+#                                                   git init
+#command: git init. we create a git repository in our local environment that we will add files init and send them to actual repository in the internet
+#
+#                                                   git add
+#command: git add {file_name}. we add files to our snapshots snapshots are the changed files that we send with commits to the repositories what we add to snapshots with add will be changed with the previous ones after the commit
+#
+#                                                   git commit
+#command: git commit -m "message to send with commit". we sent our changed file to our local repository that we have created with git init
+#
+#                                                    git status
+#command: this command will inform you if there is some unadded changes inside your files
+#
+#                                                    git remote -v
+#command: git remote -v. this command will show you the online repositories that you want to push your code if there is empty than your pushes will not work because you still dont set online repository to push your codes
+#
+#                                                    git push
+#command: git push -u {repository_name in git hub online} {branch name in the local environment}
+#
+#                                                  set url {branch_name}
+#command: git remote set-url {repository name} {https url of the repository in the git hub}. in conclusion you say computer make pushes to this https and the specified repository so when you say git push just git push the desired changes will be made to the desired projects desired repository
+#
+#
+#                                                 remote add {online repository name}
+#command: git remote add {branch_name} {https url of the git hub repository} this will open a repository in git hub online
