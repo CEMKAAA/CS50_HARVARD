0be0272579cc862d6ad0e2bb7257ec155c23bc75
unknown <ciracicemberk0@gmail.com>
2023-08-27 19:52:27 +0300

    style property has been added to the head seciton of the hello.html

--- gitCommands.py
+++ gitCommands.py
@@ -35,7 +35,7 @@
 #                                                  Merge Conflicts
 #Now here is the conflict scenerio you have made a change at one file and another person made a change at the same file when you try to pull the project from online repository the git`s system will ask you do you want to get what this another man recently changed or do you want to keep your changes or do you want to merge this two changes this is completely on you
 # 
-#                                         Here is how git system informs abput the conflict
+#                                         Here is how git system informs about the conflict
 #        Your changes 
 #        <h1>Welcome to my website!!</h1>
 #=======
@@ -43,3 +43,22 @@
 #       <h1 style="color: red; text-align:center;font-family: Arial; font-weight: bold;">Welcome to my website</h1>
 #>>>>>>> 40a706f380ef3dd6b70bdb32cd90d3ea37fe49bd
 #        At this point it is completely on you to decide whether to keep your changes or get the changes that other person has made or you can merge them
+#
+#                                                  git log
+#command: git log. explaination: we the developers works on same project make snapshots with commits in our local environments. and push this snapshots to the online project`s repositories with this git log command we can see who made pushes and when they made this pushes
+#
+#                                                 git reset
+#command: git reset --hard <commit hash>, commit hash: a unique id for every git up pushes
+#command: git reset --hard origin/master this will change your snapshot to whatever is in your online git repository currently
+#
+#-------------------------------------------------Branching----------------------------------------------------
+#
+#explaination: now imagine this scenerio you started a new web application project and made pushes pushes and pushes you have built the home page which is very good and start to make shopping page and you kept developing and make pushes pushes and pushes and eventually you realized that there is a bug at the home section of your code you want to turn back to there and you can easilly do that with git reset <commit hash> but here one point that lead us to use branches do you want to lose everything you made at shopping part of course not so here is why we need branches you finished home page part of it and will start another section of the project open a new branch named shopping and make your development and keep the home branch at one branch you will fix the bug at the home page and at the other branch you will have finished the shopping part and merge this two branches volaa your project is ready and steady with the power of branches
+#
+#                                                 git branch
+#command: git branch. this will tell you which branch are you currently on for instance master
+#
+#                                           git checkout -b {branch name}
+#command: git checkout -b {branch name} it will create a new branch and change your branch to it
+#
+#
